bd3b05969d17bc2d58cdd9061d4e494392a16610
makaveli1O <samuel.liska49@gmail.com>
2025-07-06 13:32:52 +0200

    feat: Add heart system and visuals to that

--- Assets/Scripts/GameHandler/GameOverTrigger.cs
+++ Assets/Scripts/GameHandler/GameOverTrigger.cs
@@ -1,21 +1,29 @@
 using UnityEngine;
 using Assets.Scripts.SharedKernel;
+using Assets.Scripts.HeartSystem;
 
 namespace Assets.Scripts.GameHandler{
     public class GameOverTrigger : MonoBehaviour
     {
         private string GetSceneName => SceneNames.GameOver;
         private ISceneLoader _sceneLoader;
+        private IHeartController _heartController;
 
         void Awake()
         {
             _sceneLoader = SimpleServiceLocator.Resolve<ISceneLoader>();
+            _heartController = SimpleServiceLocator.Resolve<IHeartController>();
         }
         
         private void OnCollisionEnter2D(Collision2D collision)
         {
-            if (collision.gameObject.CompareTag("Ball"))
-                _sceneLoader.LoadScene(GetSceneName);
+            if (!collision.gameObject.CompareTag("Ball"))
+                return;
+
+            _heartController.RemoveHeart();
+
+            if (_heartController.GetCurrentHeart <= 0)
+                _sceneLoader.LoadScene(GetSceneName); 
         }
     }
 }
