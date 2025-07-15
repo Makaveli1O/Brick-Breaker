5cd860228565e1bd23c91c2416457f16d0dafe76
makaveli1O <samuel.liska49@gmail.com>
2025-07-15 07:22:54 +0200

    feat: Add SlowBehaviour

---
+++ Assets/Scripts/Blocks/BlockBehaviours/SlowBehaviour.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+
+namespace Assets.Scripts.Blocks
+{
+    public class SlowBehaviour : MonoBehaviour, IConfigurableBehaviour<SlowConfig>, ICollisionBehaviour
+    {
+        public float Slowness { get; set; } = 0.5f;
+        private bool _isSlowed = false;
+        private const float _delay = 2f;
+        private const float _normalSpeed = 1f;
+        public void Configure(SlowConfig config)
+        {
+            Slowness = config.SlownessPercentage;
+        }
+
+        public void OnCollisionExecute(Block context, Collision2D collision)
+        {
+            if (_isSlowed) return;
+
+            Time.timeScale = Slowness;
+            _isSlowed = true;
+
+            context.StartCoroutine(RestoreTimeAfterDelay(_delay));
+        }
+
+        private System.Collections.IEnumerator RestoreTimeAfterDelay(float delay)
+        {
+            yield return new WaitForSecondsRealtime(delay);
+            Time.timeScale = _normalSpeed;
+            _isSlowed = false;
+        }
+    }
+}
