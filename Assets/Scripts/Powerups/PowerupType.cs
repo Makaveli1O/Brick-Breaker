8a5aa08522233a207878d294e607eaaef98b5b74
makaveli1O <samuel.liska49@gmail.com>
2025-07-18 06:55:14 +0200

    feat: Move configurable behaviour to sharedkernel

---
+++ Assets/Scripts/Powerups/PowerupType.cs
@@ -0,0 +1,14 @@
+namespace Assets.Scripts.Powerups
+{
+    public enum PowerupType
+    {
+        ExpandPaddle,
+        ShrinkPaddle,
+        ExtraLife,
+        MultiBall,
+        Laser,
+        Magnet,
+        SlowBall,
+        FastBall
+    }
+}
