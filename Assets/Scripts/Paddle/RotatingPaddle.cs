95c71f4b575956e3c81e004b3fd63d2dfaa24ecb
makaveli1O <samuel.liska49@gmail.com>
2025-07-21 13:40:25 +0200

    fix: SpeedBoost breaks rotating paddle

--- Assets/Scripts/Paddle/RotatingPaddle.cs
+++ Assets/Scripts/Paddle/RotatingPaddle.cs
@@ -6,8 +6,16 @@ public class RotatingPaddle : MonoBehaviour, IPaddleBehaviour
     private const float _defaultAngle = 0f;
     private const float _rotateLeftAngle = -45f;
     private const float _rotateRightAngle = 45f;
+    private const float _rotateSpeedMultiplier = 100f;
     private float _speed = 5f;
     private float _acceleration = 20f;
+    private float _initialAcceleration = 0f;
+    private const float _adjustedDeltaWhenActivePowerup = 15f;
+
+    void Start()
+    {
+        _initialAcceleration = Acceleration;
+    }
 
     private Rigidbody2D _rb;
     private float _targetAngle = _defaultAngle;
@@ -19,6 +27,8 @@ public class RotatingPaddle : MonoBehaviour, IPaddleBehaviour
     {
         _rb = transform.parent.GetComponent<Rigidbody2D>();
     }
+
+
 
     public void Action(InputAction.CallbackContext ctx)
     {
@@ -36,14 +46,20 @@ public class RotatingPaddle : MonoBehaviour, IPaddleBehaviour
             _targetAngle = _defaultAngle;
         }
     }
+    bool SpeedBoostPowerupActive => _initialAcceleration != Acceleration;
 
     void FixedUpdate()
     {
+        float maxDelta = _speed * _rotateSpeedMultiplier * Time.fixedDeltaTime;
+
+        if (SpeedBoostPowerupActive)
+            maxDelta = _adjustedDeltaWhenActivePowerup;
+
         // Smoothly rotate towards target angle
         float newAngle = Mathf.MoveTowardsAngle(
             _rb.rotation,
             _targetAngle,
-            _speed * 100f * Time.fixedDeltaTime
+            maxDelta
         );
 
         _rb.MoveRotation(newAngle);
